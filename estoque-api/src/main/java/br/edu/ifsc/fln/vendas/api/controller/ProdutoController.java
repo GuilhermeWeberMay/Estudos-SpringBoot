7d378f8436d7dc52e03776339e7e3b20a12cffaf
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-02-08 15:00:08 -0300

    Add GET by id endpoint to ProdutoController
    Add a ResponseEntity-based GET /Produto/{id} endpoint (pesquisar) to return a Produto by id with 200 OK or 404 Not Found. Also add several Spring Web imports (PathVariable, RequestBody, PostMapping, PutMapping, DeleteMapping, ResponseStatus, ResponseEntity, HttpStatus) in preparation for implementing create/update/delete operations.

--- estoque-api/src/main/java/br/edu/ifsc/fln/vendas/api/controller/ProdutoController.java
+++ estoque-api/src/main/java/br/edu/ifsc/fln/vendas/api/controller/ProdutoController.java
@@ -2,8 +2,16 @@ package br.edu.ifsc.fln.vendas.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import br.edu.ifsc.fln.vendas.model.domain.Produto;
 import br.edu.ifsc.fln.vendas.repository.ProdutoRepository;
@@ -19,5 +27,10 @@ public class ProdutoController {
     @GetMapping("/Produto")
     public List<Produto> listar() {
         return produtoRepository.findAll();
+    }
+
+    @GetMapping("/Produto/{id}")
+    public ResponseEntity<Produto> pesquisar (@PathVariable Integer id) {
+        return produtoRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 }
