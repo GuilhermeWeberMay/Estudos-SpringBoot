72ffe4c914ccc2d525b9ee473904d2f4b9e4bb31
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-02-08 18:50:12 -0300

    feito o endpoint de inserção

--- estoque-api/src/main/java/br/edu/ifsc/fln/vendas/api/controller/ProdutoController.java
+++ estoque-api/src/main/java/br/edu/ifsc/fln/vendas/api/controller/ProdutoController.java
@@ -1,7 +1,9 @@
 package br.edu.ifsc.fln.vendas.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import br.edu.ifsc.fln.vendas.repository.CategoriaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +17,17 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 
 import br.edu.ifsc.fln.vendas.model.domain.Produto;
 import br.edu.ifsc.fln.vendas.repository.ProdutoRepository;
+import br.edu.ifsc.fln.vendas.model.domain.Categoria;
 
 @RestController
 public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
-    public ProdutoController (ProdutoRepository produtoRepository) {
+    private CategoriaRepository categoriaRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository, CategoriaRepository categoriaRepository) {
         this.produtoRepository = produtoRepository;
+        this.categoriaRepository = categoriaRepository;
     }
 
     @GetMapping("/Produto")
@@ -30,7 +36,16 @@ public class ProdutoController {
     }
 
     @GetMapping("/Produto/{id}")
-    public ResponseEntity<Produto> pesquisar (@PathVariable Integer id) {
+    public ResponseEntity<Produto> pesquisar(@PathVariable Integer id) {
         return produtoRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/Produto")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto inserir(@RequestBody Produto produto) {
+        produto =  produtoRepository.save(produto); // Localiza a categoria
+        Optional<Categoria> c = categoriaRepository.findById(produto.getCategoria().getId());
+        produto.setCategoria(c.get());
+        return produto;
     }
 }
