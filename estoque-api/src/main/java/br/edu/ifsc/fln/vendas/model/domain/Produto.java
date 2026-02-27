065d05a26c643590774b10ef593fae73d941fe4b
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-02-27 14:15:28 -0300

    trocado para imports por extenso

--- estoque-api/src/main/java/br/edu/ifsc/fln/vendas/model/domain/Produto.java
+++ estoque-api/src/main/java/br/edu/ifsc/fln/vendas/model/domain/Produto.java
@@ -1,7 +1,16 @@
 package br.edu.ifsc.fln.vendas.model.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 import java.util.Objects;
 
