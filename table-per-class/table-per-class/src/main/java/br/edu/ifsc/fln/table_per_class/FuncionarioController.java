c742302e8ad0fc1ac7b19c3d40e3874824243a4a
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-03-02 13:45:15 -0300

    feito o verbo delete da api

--- table-per-class/table-per-class/src/main/java/br/edu/ifsc/fln/table_per_class/FuncionarioController.java
+++ table-per-class/table-per-class/src/main/java/br/edu/ifsc/fln/table_per_class/FuncionarioController.java
@@ -4,10 +4,8 @@ import br.edu.ifsc.fln.table_per_class.model.domain.Funcionario;
 import br.edu.ifsc.fln.table_per_class.repository.FuncionarioRepository;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FuncionarioController {
@@ -23,4 +21,16 @@ public class FuncionarioController {
     public Funcionario inserir(@RequestBody Funcionario funcionario) {
         return funcionarioRepository.save(funcionario);
     }
+
+    @DeleteMapping("/deleteFuncionario/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        if (!funcionarioRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }else{
+            funcionarioRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+            // noContent - devolve código 204 - sucesso, mas sem conteúdo
+        }
+    }
+
 }
